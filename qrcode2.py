b604e57cf888b9c0020dd8f6448d7ebc15547a7d
Shreyank Dattatray Patil <137429100+shreyankdp@users.noreply.github.com>
2024-02-19 23:00:26 +0530

    Update qrcode2.py

--- qrcode2.py
+++ qrcode2.py
@@ -5,5 +5,5 @@ qr=qrcode.QRCode(version=1,
                  box_size=10,border=4,)
 qr.add_data("https://www.instagram.com/shrey_ank.dp/?next=%2F")
 qr.make(fit=True)
-img=qr.make_image(fill_color="black",back_color="white")
+img=qr.make_image(fill_color="red",back_color="white")
 img.save("insta_profile2.png")
